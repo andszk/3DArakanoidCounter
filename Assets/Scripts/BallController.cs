bf68c2a9003401cd4a6c6358d59d6c133db88ef2
Andrzej Szkudlarek <szk.andrzej@gmail.com>
2020-02-16 18:16:15 +0100

    Add initial ball

---
+++ Assets/Scripts/BallController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+    private Rigidbody rigidbody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.rigidbody = this.GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.JoystickButton0))
+        {
+            Debug.Log("wohoo!!");
+            this.transform.position = new Vector3(0, 0.5f, 0);
+            this.rigidbody.velocity = new Vector3(Random.value, 5, Random.value);
+        }
+    }
+}

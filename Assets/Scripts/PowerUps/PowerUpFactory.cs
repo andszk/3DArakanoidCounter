b370eefa79f5f68b399617c1fb31d2590c23a2d5
Andrzej Szkudlarek <szk.andrzej@gmail.com>
2020-06-01 20:48:31 +0200

    Create random powerUp based on Assembly

--- Assets/Scripts/PowerUps/PowerUpFactory.cs
+++ Assets/Scripts/PowerUps/PowerUpFactory.cs
@@ -1,11 +1,18 @@
 ﻿using Assets.Scripts;
 using Assets.Scripts.PowerUps;
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
 
 internal class PowerUpFactory
 {
+    private static List<TypeInfo> powerUps = Assembly.GetExecutingAssembly().DefinedTypes.Where(t => t.BaseType == typeof(PowerUp)).ToList();
+    private static Random rnd = new Random();
+
     internal static PowerUp GetRandomPowerUp()
     {
-        return new DoublePadSize();
+        var index = rnd.Next(powerUps.Count);
+        return Activator.CreateInstance(powerUps[index]) as PowerUp;
     }
 }
